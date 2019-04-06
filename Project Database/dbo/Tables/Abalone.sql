580fd87d1fed6431d7b2e1a629ccb0dc23a5dcd5
Łukasz Zborek <luki215@gmail.com>
2019-04-06 12:40:04 +0200

    PROJEKT 01

---
+++ Project Database/dbo/Tables/Abalone.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Abalone] (
+    [Sex]           VARCHAR (1)    NULL,
+    [Length]        NUMERIC (8, 4) NULL,
+    [Diameter]      NUMERIC (8, 4) NULL,
+    [Height]        NUMERIC (8, 4) NULL,
+    [WholeWeight]   NUMERIC (8, 4) NULL,
+    [ShuckedWeight] NUMERIC (8, 4) NULL,
+    [VisceraWeight] NUMERIC (8, 4) NULL,
+    [ShellWeight]   NUMERIC (8, 4) NULL,
+    [Rings]         SMALLINT       NULL
+);
+
---
+++ Project Database/dbo/Tables/Cars.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Cars] (
+    [Buying]  VARCHAR (5) NULL,
+    [Maint]   VARCHAR (5) NULL,
+    [Doors]   VARCHAR (5) NULL,
+    [Persons] VARCHAR (4) NULL,
+    [LugBoot] VARCHAR (5) NULL,
+    [Safety]  VARCHAR (4) NULL,
+    [UNACC]   VARCHAR (5) NULL
+);
+
